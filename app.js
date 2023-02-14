2dbff037d69f4edb9dfbdc5310b663240068e8c8
Sherifdeen Ishola <isholasherifdeen@gmail.com>
2023-02-14 14:17:56 +0100

    ...

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
-const blogRoutes = require("./routes/blogRoutes")
+const blogRoutes = require("./routes/blogRoutes");
 
 //Express App
 const app = express();
@@ -11,29 +11,34 @@ app.use(express.static("public"));
 app.use("/blogs", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-  
+
 // Register view engines
 app.set("view engine", "ejs");
 
 // Connecting to the database
-mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(process.env.PORT, console.log("Database Connected")))
-    .catch((err) => console.log(err));
+mongoose
+  .connect(process.env.dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) =>
+    app.listen(5500 || process.env.PORT, console.log("Database Connected"))
+  )
+  .catch((err) => console.log(err));
 
 // Routes
 app.get("/", (req, res) => {
-    res.redirect("/blogs")
-})
+  res.redirect("/blogs");
+});
 
 app.get("/about", (req, res) => {
-    res.render("about", { title : "About" })
+  res.render("about", { title: "About" });
 });
 
-// blog routes 
-app.use("/blogs", blogRoutes)
+// blog routes
+app.use("/blogs", blogRoutes);
 
-// Error 404 
-app.use((req,res) => {
-    res.status(404).render("404", {title : "404"})
+// Error 404
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
 });
-
